ea9d9039df5a8fbfaa77028d6d1dc9b904a92301
Dennis Shevtsov <dennis.shevtsov.dev@gmail.com>
2024-01-18 11:46:21 -0800

    add quartz

--- src/QuartzSample.Web/Program.cs
+++ src/QuartzSample.Web/Program.cs
@@ -1,4 +1,10 @@
+using Quartz;
+using Quartz.AspNetCore;
+
 WebApplicationBuilder builder = WebApplication.CreateBuilder(args);
+builder.Services.AddQuartz(configurator => configurator.UsePersistentStore(options => options.UsePostgres("")));
+builder.Services.AddQuartzServer();
+
 WebApplication app = builder.Build();
 app.MapGet("/", () => "Quartz sample working...");
 app.Run();
