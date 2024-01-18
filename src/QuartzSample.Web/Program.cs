d89028e72dd144c3291e0f154829b13a3a13e98e
Dennis Shevtsov <dennis.shevtsov.dev@gmail.com>
2024-01-18 11:57:48 -0800

    add postgres as persist store

--- src/QuartzSample.Web/Program.cs
+++ src/QuartzSample.Web/Program.cs
@@ -2,7 +2,16 @@ using Quartz;
 using Quartz.AspNetCore;
 
 WebApplicationBuilder builder = WebApplication.CreateBuilder(args);
-builder.Services.AddQuartz(configurator => configurator.UsePersistentStore(options => options.UsePostgres("")));
+builder.Services.AddQuartz(configurator =>
+{
+  configurator.UsePersistentStore(options =>
+  {
+    string connectionString = builder.Configuration.GetConnectionString("QUARTZ") ??
+                              throw new ArgumentNullException("QUARTZ");
+    options.UsePostgres(connectionString);
+    options.UseNewtonsoftJsonSerializer();
+  });
+});
 builder.Services.AddQuartzServer();
 
 WebApplication app = builder.Build();
